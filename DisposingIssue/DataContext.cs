e5ee5d13682c031f966925a348dc6768b74dfd98
Quan Nguyen <quann@betenbough.com>
2021-01-12 11:42:28 -0600

    Add project files.

---
+++ DisposingIssue/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposingIssue
+{
+    class DataContext : IDisposable
+    {
+        private readonly string _InstanceName;
+
+        bool disposed;
+
+        public DataContext(string instanceName)
+        {
+            _InstanceName = instanceName;
+            disposed = false;
+        }
+
+        public void PrintMyName()
+        {
+            if (disposed)
+                throw new ObjectDisposedException(_InstanceName);
+
+            Console.WriteLine(_InstanceName);
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"{_InstanceName} is disposed.");
+            disposed = true;
+        }
+    }
+}
---
+++ DisposingIssue/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DisposingIssue
+{
+    class Program
+    {
+        static DataContext _Db;
+
+        static void Main(string[] args)
+        {
+            var thread = new Thread(() => RunThisInSeparateThread());
+            thread.Start();
+
+            using (_Db = new DataContext("Main context"))
+            {
+                RunThisInMainThread();
+            }
+        }
+
+        static void RunThisInSeparateThread()
+        {
+            using (var db = new DataContext("Separate context"))
+            {
+                Thread.Sleep(2000);
+                db.PrintMyName();
+            }
+        }
+
+        static void RunThisInMainThread()
+        {
+            _Db.PrintMyName();
+        }
+    }
+}
