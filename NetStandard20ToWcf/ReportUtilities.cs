3c5cb6c4783074b392f7ac6cf667c61c40583337
Quan Nguyen <quann@betenbough.com>
2021-01-14 14:24:51 -0600

    Test WCF on Net Standard 2.0

---
+++ NetStandard20ToWcf/ReportUtilities.cs
@@ -0,0 +1,28 @@
+﻿using ReportingService2005;
+using System;
+using System.Net;
+using System.Security.Principal;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace NetStandard20ToWcf
+{
+    public class ReportUtilities
+    {
+        public async Task<CatalogItem[]> GetReportsAsync()
+        {
+            var bind = new BasicHttpBinding();
+            bind.Security.Mode = BasicHttpSecurityMode.TransportCredentialOnly;
+            bind.Security.Transport.ClientCredentialType = HttpClientCredentialType.Ntlm;
+            bind.MaxReceivedMessageSize = 64000000;
+
+            var endpoint = new EndpointAddress("http://sqllive/ReportServer/ReportService2005.asmx");
+
+            var service = new ReportingService2005SoapClient(bind, endpoint);
+            service.ClientCredentials.Windows.AllowedImpersonationLevel = TokenImpersonationLevel.Impersonation;
+            service.ChannelFactory.Credentials.Windows.ClientCredential = CredentialCache.DefaultNetworkCredentials;
+
+            return (await service.ListChildrenAsync("/Betenbough", true)).CatalogItems;
+        }
+    }
+}
---
+++ TestWcfOnNetStandard/Program.cs
@@ -0,0 +1,17 @@
+﻿using NetStandard20ToWcf;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWcfOnNetStandard
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var report = new ReportUtilities();
+            var catalogItems = await report.GetReportsAsync();
+            Console.WriteLine(string.Join("\n", catalogItems.Select(i => i.Name).ToList()));
+        }
+    }
+}
